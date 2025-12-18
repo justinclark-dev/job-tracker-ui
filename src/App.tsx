cfb9443c8f07942ef338ee849c1aa83519adef27
justinclark-dev <justinclark.dev@gmail.com>
2025-12-17 21:17:04 -0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <section>
+          <nav>
+            <ul>
+              <li>
+                HOME
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </header>
+      <main>
+        <section>
+          <h1>Main Section</h1>
+        </section>
+      </main>
+      <footer>
+        <section>
+          @footer
+        </section>
+      </footer>
+    </>
+  )
+}
+
+export default App
